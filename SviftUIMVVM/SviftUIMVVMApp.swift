3be1477c5b7dd89b541ab1fb5d353d954b69e81a
B0Dim <bodim@me.com>
2022-03-08 14:26:13 +0300

    Initial Commit

---
+++ SviftUIMVVM/SviftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SviftUIMVVMApp.swift
+//  SviftUIMVVM
+//
+//  Created by  BoDim on 08.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct SviftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
